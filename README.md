d5db82a132efaab65de58cbbeecf921fa67b5c7a
Ingo Kleiber <ikleiber@googlemail.com>
2021-11-12 13:58:37 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# custom_autorecon_plugins
+# Custom AutoRecon Plugins
+
+This is a small collection of my (public) plugins for Tib3rius' fantastic [AutoRecon](https://github.com/Tib3rius/AutoRecon/).
+
+* `rustscan.py` -> QuickRustScanScan
+
+## Additional Requirements
+
+In addition to `AutoRecon` and all its requirements, you will need:
+
+```
+rustscan
+```
+
+## Usage
+
+`AutoRecon` has a beautiful and simple-to-use plugin system. On my `Kali` box, I keep my plugins in `/opt/custom_autorecon_plugins`.
+To run the tool using my plugins in addition to the regular ones, I use:
+
+`sudo autorecon IP --add-plugins-dir /opt/custom_autorecon_plugins`
---
+++ rustscan.py
@@ -0,0 +1,23 @@
+from autorecon.plugins import PortScan
+from autorecon.io import info, error
+from autorecon.config import config
+import os, re
+
+class QuickRustScanScan(PortScan):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Quick RustScan'
+        self.description = 'Performs a basic RustScan scan.'
+        self.type = 'tcp'
+        self.tags = ['default', 'additional-port-scan']
+        self.priority = 0
+
+    async def run(self, target):
+        if target.ports: # Don't run this plugin if there are custom ports.
+            return []
+
+        process, stdout, stderr = await target.execute('rustscan -a {address} -g --ulimit 5000 > "{scandir}/_quick_rustscan.txt"', blocking=False)
+        services = await target.extract_services(stdout)
+        await process.wait()
+        return services
